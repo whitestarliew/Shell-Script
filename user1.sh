2d21a05b471cb3012f8cb209a322008d2d7ffd56
whitestarliew <51337039+whitestarliew@users.noreply.github.com>
2021-08-26 10:02:05 +0800

    Create a new file
    none.

---
+++ user1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#This script creates an acc on the local system.
+#you will be prompted for the acc name and password
+
+#ask for the user name.
+read -p "enter the username to create: " USER_NAME
+
+#ask for the real name.
+read -p "enter the name of the person who this account is for: " COMMENT
+
+#ask password
+read -p "Enter the password for the accont: " PASSWORD
+
+#create user .
+useradd -c "${COMMENT}" -m ${USER_NAME}
+#setup password for the user
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+#Force password to change at first login
+passwd -e ${USER_NAME}
