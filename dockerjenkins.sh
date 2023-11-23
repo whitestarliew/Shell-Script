4eba4e6da41964b2e6c99cc7576d1984885c8505
Ralston <51337039+whitestarliew@users.noreply.github.com>
2023-11-23 23:20:33 +0800

    Update dockerjenkins.sh

--- dockerjenkins.sh
+++ dockerjenkins.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Set your source Docker registry and AWS ECR registry details
-SOURCE_REGISTRY="https://abc/nexus3"
-AWS_REGION="your-aws-region"
-AWS_ACCOUNT_ID="your-aws-account-id"
+# Prompt the user for the source Nexus registry
+read -p "Enter the source Nexus repository URL (e.g., https://abc/nexus3/repository/ABC-Containers): " SOURCE_REGISTRY
 
-# Prompt the user for image digest
-read -p "Enter the image digest (e.g., sha256:blablablablabla): " IMAGE_DIGEST
+# Validate input
+if [ -z "$SOURCE_REGISTRY" ]; then
+  echo "Error: Source Nexus repository URL is required."
+  exit 1
+fi
+
+# Prompt the user for the image digest
+read -p "Enter the image digest (e.g., sha256:fgregtrahgyshjthtgtbb): " IMAGE_DIGEST
 
 # Validate input
 if [ -z "$IMAGE_DIGEST" ]; then
@@ -14,14 +18,17 @@ if [ -z "$IMAGE_DIGEST" ]; then
   exit 1
 fi
 
-# Pull the image from the source registry using the digest
-docker pull $SOURCE_REGISTRY@$IMAGE_DIGEST
+# Pull the image from the source repository using the full URL to the digest
+docker pull $SOURCE_REGISTRY/v2/blobs/$IMAGE_DIGEST
+
+# AWS region for ECR
+AWS_REGION="eu-west-2"
 
 # Authenticate with AWS ECR
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 # Tag the pulled image for AWS ECR
-docker tag $SOURCE_REGISTRY@$IMAGE_DIGEST $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_ECR_REPO:$IMAGE_DIGEST
+docker tag $SOURCE_REGISTRY/v2/blobs/$IMAGE_DIGEST $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_ECR_REPO:$IMAGE_DIGEST
 
 # Push the image to AWS ECR
 if docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_ECR_REPO:$IMAGE_DIGEST; then
